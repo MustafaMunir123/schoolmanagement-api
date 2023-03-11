6605996319910555eea90fadb1d43cf9b5b8ec51
MustafaMunir123 <mustafamunir10@gmail.com>
2023-03-12 00:11:46 +0500

    feat: modified user model

--- schoolmanagement_api/apps/users/models.py
+++ schoolmanagement_api/apps/users/models.py
@@ -1,18 +1,19 @@
-from datetime import datetime
+from datetime import date
 
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 USER_ROLE_CHOICES = (
-    ("Teacher", "Teacher"),
-    ("Finance", "Finance"),
+    ("Admin", "Admin"),
+    ("Owner", "Owner"),
 )
 
 
 class SchoolUser(models.Model):
     first_name = models.CharField(max_length=150, blank=False, null=False)
     last_name = models.CharField(max_length=150, blank=False, null=False)
-    email = models.EmailField(blank=False, null=False)
+    email = models.EmailField(blank=False, null=False, unique=True)
     role = models.CharField(
         max_length=70, choices=USER_ROLE_CHOICES, null=False, blank=False
     )
@@ -23,7 +24,10 @@ class SchoolUser(models.Model):
             "Unselect this instead of deleting accounts."
         ),
     )
-    date_joined = models.DateTimeField(default=datetime.today())
+    date_joined = models.DateField(default=date.today)
+    phonenumber = PhoneNumberField(max_length=13, blank=False, null=False, unique=True)
+    username = models.CharField(unique=True, max_length=25, blank=False, null=False)
+    password = models.CharField(max_length=20, blank=False, null=False)
 
     def __str__(self):
         return str(self.first_name) + " " + str(self.last_name)

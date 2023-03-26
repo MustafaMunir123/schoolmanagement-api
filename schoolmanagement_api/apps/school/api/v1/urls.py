4a540b8e6c0db2269e5c2049feeb63547bc3d3a9
MustafaMunir123 <mustafamunir10@gmail.com>
2023-03-26 16:40:55 +0500

    chore: add urls route for teacher api

--- schoolmanagement_api/apps/school/api/v1/urls.py
+++ schoolmanagement_api/apps/school/api/v1/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from schoolmanagement_api.apps.school.api.v1.views import SubjectApiView
+from schoolmanagement_api.apps.school.api.v1.views import SubjectApiView, TeacherApiView
 
 urlpatterns = [
     path("v1/subject/", SubjectApiView.as_view()),
+    path("v1/teacher/", TeacherApiView.as_view()),
+    path("v1/teacher/get/", TeacherApiView.as_view()),
+    path("v1/teacher/get/<uuid:pk>", TeacherApiView.as_view()),
+    path("v1/teacher/update/<uuid:pk>", TeacherApiView.as_view()),
 ]
